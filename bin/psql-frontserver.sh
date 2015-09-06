5c04479279afec3afe9907350cd20f015af87093
Aditya Prasad <aytida77@gmail.com>
2015-09-06 08:24:01 +0000

    Shifted frontserver and notificationserver to new db

--- bin/psql-frontserver.sh
+++ bin/psql-frontserver.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-psql --host=aws.c1592uu9ef4m.us-east-1.rds.amazonaws.com --port=5432 --username=lancelot --password --dbname=reaper_frontserver
+psql --host=aws.cbofuivtssmn.us-west-2.rds.amazonaws.com --port=5432 --username=lancelot --password --dbname=reaper_frontserver
