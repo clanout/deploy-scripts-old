354d43609395db2cd4d8f4258879981dcaef8acb
Aditya Prasad <aytida77@gmail.com>
2015-09-06 12:40:17 +0000

    db fix

--- bin/psql-frontserver.sh
+++ bin/psql-frontserver.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-psql --host=aws.cbofuivtssmn.us-west-2.rds.amazonaws.com --port=5432 --username=lancelot --password --dbname=reaper_frontserver
+psql --host=aws.cjmgf73hg2wc.us-east-1.rds.amazonaws.com --port=5432 --username=lancelot --password --dbname=reaper_frontserver
--- bin/psql-notificationserver.sh
+++ bin/psql-notificationserver.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-psql --host=aws.cbofuivtssmn.us-west-2.rds.amazonaws.com --port=5432 --username=lancelot --password --dbname=reaper_notificationserver
+psql --host=aws.cjmgf73hg2wc.us-east-1.rds.amazonaws.com --port=5432 --username=lancelot --password --dbname=reaper_notificationserver
